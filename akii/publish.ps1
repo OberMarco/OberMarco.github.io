784779e06f73ea3c344593e7baabb4c68bb7a17f
Marco Obermeier <obermeier20194@hs-ansbach.de>
2024-02-01 09:34:21 +0100

    Commit on 02/01/2024 09:34:21

--- akii/publish.ps1
+++ akii/publish.ps1
@@ -1,6 +1,9 @@
+Set-Location "C:\Users\marco\Programme\mapr\3_AI_course_2\3_Projekt\4_presentation"
+jupyter-nbconvert --to slides presentation.ipynb --SlidesExporter.reveal_theme=white 
 Remove-Item -Recurse -Force "C:\Users\marco\Programme\githubpage\akii"
 Copy-Item -Recurse "C:\Users\marco\Programme\mapr\3_AI_course_2\3_Projekt" "C:\Users\marco\Programme\githubpage\akii"
 Set-Location "C:\Users\marco\Programme\githubpage\akii"
 git add .
 git commit -m "Commit on $(Get-Date)"
 git push
+Set-Location "C:\Users\marco\Programme\mapr\3_AI_course_2\3_Projekt"
